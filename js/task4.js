d9a32c02a42991a1f84bc1cbd702cba8db3518df
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-01-12 01:39:47 +0800

    task 4 done

---
+++ js/task4.js
@@ -0,0 +1,3 @@
+let souvenirWeight = Number(prompt("How many souvenirs do you have?"));
+let toyWeight = Number(prompt("How many toys do you have?"));
+alert("Total weight: " + (souvenirWeight * 75 + toyWeight * 112) / 1000 + " kg");

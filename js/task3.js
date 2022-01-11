51d428f877b8e8880f8dd5a700ac06b96731a774
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-01-12 00:17:40 +0800

    task 3 done

---
+++ js/task3.js
@@ -0,0 +1,4 @@
+alert("Click OK to see my name and email");
+let userName = "Ilgiz Zhaparov";
+let userMail = "ilgiz.zhaparov.on@gmail.com";
+alert("Name: " + userName + " and" + " Email: " + userMail);
